ee4ea5d1a358c6f2eeb06afa818782ea1037884e
Madhav008 <madhavj211@gmail.com>
2023-12-10 14:58:11 +0530

    Button changed

--- vite-project/src/Pages/AuthForm.tsx
+++ vite-project/src/Pages/AuthForm.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { cn } from '@/lib/utils'
+import { Link } from 'react-router-dom'
 
 const AuthForm = () => {
     return (
@@ -34,7 +35,15 @@ const AuthForm = () => {
                             autoCorrect="off"
                         />
                     </div>
-                    <Button disabled={false}>Sign In with Email</Button>
+                    {/* <Button disabled={false}>Sign In with Email</Button> */}
+                    <Link
+
+                        to="/home"
+                        className={cn("inline-flex items-center justify-center h-11  px-8 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90"
+                        )}
+                    >
+                        Sign In with Email
+                    </Link>
                 </div>
             </form>
         </div>
