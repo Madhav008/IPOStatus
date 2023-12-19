78f6022c1d95a45f217dbc8da5c60360f38ea096
Madhav008 <madhavj211@gmail.com>
2023-12-19 20:06:52 +0530

    removed the comma

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
       <Provider store={store}>
         <App />
-      </Provider>,
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>,
 )
