5b96768e0098f3ebb8b10af289f3e2acd67ac339
Madhav008 <madhavj211@gmail.com>
2023-12-23 16:29:14 +0530

    start change

--- vite-project/start.sh
+++ vite-project/start.sh
@@ -7,5 +7,5 @@ npm run build
 docker build -t ipostatus-frontend .
 
 docker stop ipostatus-container
-docker rm  ipostaus-container  # Remove the existing container if it exists
+docker rm  ipostatus-container  # Remove the existing container if it exists
 docker run -d --name ipostatus-container -p 5123:5125 ipostatus-frontend  # Start a new container
