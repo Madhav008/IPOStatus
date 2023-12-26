b176f7856aff142d1e3e910e0806473fd284729e
Madhav008 <madhavj211@gmail.com>
2023-12-26 23:39:03 +0530

    karvy rolback done

--- sites/karvy.js
+++ sites/karvy.js
@@ -6,7 +6,7 @@ import cheerio from 'cheerio'
 import { exec, execSync } from 'child_process';
 import { logger } from '../logger.js';
 import { cacheData, checkIfCached } from '../controllers/cacheController.js';
-
+import puppeteer from 'puppeteer';
 const getKarvyIpoList = async () => {
     try {
         const response = await axios.get('https://kprism.kfintech.com/ipostatus/');
@@ -185,6 +185,7 @@ const getCaptcha = async () => {
 
 let isCaptcha = false
 let captchaCode;
+/* 
 async function processPan(PAN, company_id) {
     let data;
     if (!isCaptcha) {
@@ -207,6 +208,149 @@ async function processPan(PAN, company_id) {
         return null
     }
 
+}
+*/
+const processPan = async (PAN, company_id, maxRetries = 3) => {
+
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: "new",
+        // headless: false
+    });
+
+    const page = await browser.newPage();
+    // page.setDefaultTimeout(2000);
+    await page.setViewport({ width: 1000, height: 800 });
+
+    await page.goto('https://kosmic.kfintech.com/ipostatus/');
+
+
+    for (let retryCount = 0; retryCount < maxRetries; retryCount++) {
+
+        const divSelector = 'img#captchaimg';
+
+        await page.waitForSelector(divSelector);
+
+
+        const divBoundingBox = await page.$eval(divSelector, div => {
+            const { x, y, width, height } = div.getBoundingClientRect();
+            return { x, y, width, height };
+        });
+
+        // Check if the width is positive
+        if (divBoundingBox.width <= 0) {
+            console.error(`Error processing PAN ${PAN}: Width of the bounding box is not positive`);
+            divBoundingBox.width = divBoundingBox.width + 20
+        }
+        const randomNumber = Math.random().toString(36).substr(2, 10);
+
+        // Get the current timestamp and convert it to a string
+        const timestamp = new Date().getTime().toString(36);
+
+        // Combine the random number and timestamp to create a unique ID
+        const randomId = randomNumber + timestamp;
+        // Capture screenshot of the div
+
+        let imgPath = `screnshot_${randomId}.png`
+        await page.screenshot({
+            path: imgPath,
+            clip: {
+                x: divBoundingBox.x,
+                y: divBoundingBox.y,
+                width: divBoundingBox.width,
+                height: divBoundingBox.height,
+            },
+        });
+
+        // Create the worker outside the inner function
+        const captchaCode = await decodeCaptcha(imgPath)
+
+        console.log('Screenshot captured successfully!');
+
+
+        if (captchaCode) {
+
+            await page.select('#ddl_ipo', company_id);
+            await page.click('#pan');
+            await page.type('#txt_pan', PAN);
+
+
+            await page.type('#txt_captcha', captchaCode + '');
+
+
+            await page.click('#btn_submit_query');
+
+            try {
+                await page.waitForSelector('#hdr_ipo', { timeout: 2000 });
+
+                const data = await page.evaluate(() => {
+                    const rowData = {};
+
+                    // Select the container with class "successbox_md"
+                    const container = document.querySelector('.successbox_md');
+
+                    // Check if the container is found
+                    if (container) {
+                        // Select elements within the container using querySelector or querySelectorAll
+                        const applicationNumber = container.querySelector('#grid_results_ctl02_l1');
+                        const category = container.querySelector('#grid_results_ctl02_Label1');
+                        const name = container.querySelector('#grid_results_ctl02_Label2');
+                        const clientId = container.querySelector('#grid_results_ctl02_lbl_dpclid');
+                        const pan = container.querySelector('#grid_results_ctl02_lbl_pan');
+                        const applied = container.querySelector('#grid_results_ctl02_Label5');
+                        const allotted = container.querySelector('#grid_results_ctl02_lbl_allot');
+
+                        // Check if elements are found
+                        if (applicationNumber && category && name && clientId && pan && applied && allotted) {
+                            rowData['Application Number'] = applicationNumber.textContent.trim();
+                            rowData['Category'] = category.textContent.trim();
+                            rowData['Name'] = name.textContent.trim();
+                            rowData['Client ID'] = clientId.textContent.trim();
+                            rowData['PAN'] = pan.textContent.trim();
+                            rowData['Applied'] = parseInt(applied.textContent.trim());
+                            rowData['Alloted'] = parseInt(allotted.textContent.trim());
+                        }
+                    }
+
+                    return rowData;
+                });
+
+                await page.click('#lnk_new');
+                await browser.close()
+                console.log(data)
+                return data
+
+            } catch (error) {
+
+                console.log('Confirmation box appeared or timeout occurred. Handling this scenario...');
+
+                const confirmationText = await page.evaluate(() => {
+                    const confirmationBox = document.querySelector('.jconfirm-content');
+                    return confirmationBox ? confirmationBox.textContent.trim() : null;
+                });
+
+                console.log("Processing PAN " + PAN)
+
+
+                if (confirmationText.toLowerCase().includes('captcha')) {
+                    console.log(`Retrying PAN ${PAN} due to CAPTCHA error. Retry count: ${retryCount + 1}`);
+                    // Optionally, you can add a delay before retrying to avoid being blocked
+                    await page.click('.jconfirm-buttons')
+                    await page.waitForTimeout(1000);
+                    console.log('Confirmation text:', confirmationText);
+                } else {
+                    console.log('Confirmation text: line 171: ', confirmationText);
+                    await page.click('.jconfirm-buttons');
+                    await browser.close()
+                    return ({ PAN: PAN, QTY: confirmationText })
+                }
+            }
+
+
+        }
+    }
+    await browser.close()
+    return ({ PAN: PAN, QTY: "Not Able Process this PAN" })
 }
 
 const karvyCaptcha = async (PAN, company_id = "INOL~inox_indiapleqfv2~0~20/12/2023~20/12/2023~EQT") => {
